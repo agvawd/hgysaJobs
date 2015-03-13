a97d55a77e176eb4370f23bd4921c727f63196db
Allen <agvawd@hotmail.com>
2015-03-12 21:01:13 -0600

    updated the login and alert messages if email or password is wrong

--- js/templates/login/loginCtrl.js
+++ js/templates/login/loginCtrl.js
@@ -2,29 +2,45 @@ angular.module("hgysaJobs").controller("loginCtrl", function($scope, mainService
 	var firebaseUrl = env.getAppUrl();
 
 	$scope.logMeIn = function(existUser) {
-		mainService.logUserIn(existUser.email, existUser.password).then(function(currentAuth){
-			console.log(currentAuth);
-			if(currentAuth){
-				$location.path("/addjob")
-			}
-			else{
-				$location.path("/login")
-			}
-		})
-		$scope.existUser = '';
+		if($scope.existUser.email === undefined || $scope.existUser.password === undefined || $scope.existUser === undefined){
+			$scope.logInAlert = true;
+		}
+		else {	
+			mainService.logUserIn(existUser.email, existUser.password).then(function(currentAuth){
+				console.log(currentAuth);
+				if(currentAuth){
+					$location.path("/addjob")
+				}
+				else{
+					$location.path("/login")
+				}
+			})
+		}
+			$scope.existUser = '';
 	}
 
 	$scope.addUser = function(newUser) {
-		debugger;
-		mainService.addUser(newUser.email, newUser.password).then(function(currentAuth){
-			console.log("hit")
-			if(currentAuth){
-				$location.path("/addjob")
-			}
-			else{
-				$location.path("/login")
-			}
-		});
-		$scope.newUser = '';
+		if($scope.newUser.email === undefined || $scope.newUser.password === undefined || $scope.newUser === undefined) {
+			$scope.logUserIn = true;
+		}
+		else if ($scope.newUser.password !== $scope.conPass){
+			$scope.newUserAlert = true;
+		}
+		else {
+			mainService.addUser(newUser.email, newUser.password).then(function(currentAuth){
+				if(currentAuth){
+					$location.path("/addjob")
+				}
+				else{
+					$location.path("/login")
+				}
+			});
+		}
+			$scope.newUser = '';
+			$scope.conPass = '';
+	}
+	
+	$scope.isClicked = function(){
+		document.querySelector("#myCard").classList.toggle("flip");
 	}
 })
